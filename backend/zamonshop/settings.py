9693d326da4511af8ea541b70bf86dcccc8d84cb
AkbarAli <88106359+codewithakbar@users.noreply.github.com>
2023-05-03 00:31:45 +0500

    static fix bug :angry:

--- backend/zamonshop/settings.py
+++ backend/zamonshop/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+import os
 
 from datetime import timedelta
 from pathlib import Path
@@ -131,7 +132,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [ BASE_DIR / "static"]
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static/"),
+)
+
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # STATICFILES_DIRS = (
