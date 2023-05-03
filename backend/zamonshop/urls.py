a6159b988fb7c62270c19639e6ed774beefaa1f7
Satipov Akbar <88106359+codewithakbar@users.noreply.github.com>
2023-05-03 18:24:22 +0500

    static :underage:

--- backend/zamonshop/urls.py
+++ backend/zamonshop/urls.py
@@ -28,8 +28,7 @@ urlpatterns = [
     path('users/', include('users.urls', namespace="users")),
     path('orders/', include('orders.urls', namespace="orders")),
 
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += [re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT, }), ]
